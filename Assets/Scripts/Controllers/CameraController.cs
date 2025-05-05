8eb8b3b59e2e453e9423d5cd9d01fe8af1fc134a
KyungMin Nam <scr08212@gmail.com>
2025-05-05 23:48:52 +0900

    WIP

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -8,14 +8,35 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     float _rotationSpeed = 2f;
 
+    float _cameraCollisionOffset = 0.2f;
+    float _minCollisionOffset = 0.2f;
+
     [SerializeField]
     Transform _follow;
+
+    [SerializeField]
+    LayerMask _collisionMask;
+
+    Transform _pivot;
+    Transform _camera;
+    Vector3 _defaultPosition;
+
+
+    void Awake()
+    {
+        _camera = GetComponentInChildren<Camera>().transform;
+        _pivot = _camera.parent;
+        _defaultPosition = _camera.localPosition;
+    }
+
     void FixedUpdate()
     {
         float delta = Time.fixedDeltaTime;
 
         if(_follow != null )
             FollowTarget(delta);
+
+        HandleCameraCollision(delta);
     }
 
     void FollowTarget(float delta)
@@ -26,4 +47,24 @@ public class CameraController : MonoBehaviour
         transform.position = targetPosition;
         transform.rotation = targetRotation;
     }
+
+    void HandleCameraCollision(float delta)
+    {
+        float targetPos = _defaultPosition.z;
+        Vector3 dir = (_camera.position - _pivot.position).normalized;
+        Debug.DrawRay(_pivot.position, dir * Mathf.Abs(_defaultPosition.z), Color.red, 1f);
+        if (Physics.Raycast(_pivot.position, dir, out RaycastHit hit, Mathf.Abs(targetPos), _collisionMask))
+        {
+            float distance = Vector3.Distance(_pivot.position, hit.point);
+            targetPos = -(distance - _cameraCollisionOffset);
+        }
+        if (Mathf.Abs(targetPos) < _minCollisionOffset)
+        {
+            targetPos = -_minCollisionOffset;
+        }
+
+        targetPos = Mathf.Lerp(_camera.localPosition.z, targetPos, delta / 0.2f);
+        _camera.localPosition = new Vector3(0, _defaultPosition.y, targetPos);
+    }
+
 }
